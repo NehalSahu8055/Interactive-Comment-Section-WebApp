6b2c8e517049e5cdc7cb8d24f97c7d4ef410424d
Nehal_Sahu <ProfessionalNehal@outlook.com>
2023-10-31 20:36:16 +0530

    Fix delete function issue

--- src/components/MainContent/Comment/Cards/ReusableCard.jsx
+++ src/components/MainContent/Comment/Cards/ReusableCard.jsx
@@ -63,9 +63,6 @@ export default function ReusableCard({ person, type }) {
   };
 
   const editComment = (e) => {
-    // {
-    //   console.log("Submit initiated : ", isEditing);
-    // }
     const { name, value, maxLength } = e.target;
 
     const charCount = value.trim().length;
@@ -128,178 +125,183 @@ export default function ReusableCard({ person, type }) {
 
   console.log(isReplying);
 
-  // <ReusableCard person={top} type={"update"} />
   return (
-    <>
-      {!isDeleting && (
-        <div
-          key={id}
-          id={id}
-          className={`user-comment ${
-            commentError ? "animate-shake" : ""
-          }  space-y-4  rounded-lg bg-whitee  p-4 dark:bg-d-whitee`}
-        >
-          {commentType === "update" && (
-            <div className="flex items-center">
-              <a
-                href="#"
-                className="cursor-pointer rounded-full p-1 transition-all duration-500 hover:bg-light-gray dark:hover:bg-d-light-gray"
-                aria-labelledby="update-userID"
-              >
-                <span className="sr-only" id="update-userID">
-                  Click this to view profile
-                </span>
-                <figure className="flex items-center gap-4 pr-1  font-medium">
-                  <img
-                    className="w-8 rounded-full"
-                    src={image}
-                    alt={`User Avatar ${username}`}
-                  />
-                  <figcaption className="text-dark-blue dark:text-d-dark-blue">
-                    {username}
-                  </figcaption>
-                </figure>
-              </a>
-              {isCurrentUser && (
-                <div className="h-fit rounded-sm bg-moderate-blue px-1.5 text-sm text-whitee dark:bg-[#676ad1]">
-                  you
-                </div>
-              )}
-              <span className="pl-4 text-grayish-blue dark:text-white">
-                {createdAt}
-              </span>
-            </div>
-          )}
-
-          {/* Handles textarea editing */}
-          {isCurrentUser &&
-          (commentType === "update" ? isEditing : !isEditing) ? (
-            <form className="relative" onSubmit={updateComment}>
-              <textarea
-                id="commentTextAreaID"
-                name="comment"
-                className={`comment-editing flex h-[10rem] w-full resize-none items-center justify-center overflow-auto rounded-md border dark:bg-[#383a40] ${
-                  commentError
-                    ? "border-red-500"
-                    : "focus:border-moderate-blue dark:focus:border-d-moderate-blue"
-                }  p-2 text-grayish-blue caret-moderate-blue outline-none dark:text-white dark:caret-d-moderate-blue `}
-                value={comment}
-                onChange={editComment}
-                onKeyDown={handleKeyDown}
-                placeholder="Add a comment..."
-                maxLength={MAX_CHARS_COMMENT}
-                autoFocus
-              />
-
-              {/* Handles emoji picker */}
-              {showEmoji && (
-                <div className="absolute right-2 top-full z-10 bg-very-light-gray shadow-xl  dark:bg-d-very-light-gray">
-                  <Picker
-                    data={emojidata}
-                    onEmojiSelect={addEmoji}
-                    emojiButtonSize={30}
-                    emojiSize={20}
-                    navPosition="bottom"
-                    previewPosition="none"
-                    skinTonePosition="none"
-                    emojiButtonColors={["skyblue", "lightgreen", "violet"]}
-                  />
-                </div>
-              )}
-              <small className="absolute -top-4 right-3 text-slate-400 ">
-                <i>{charCount}</i>
-              </small>
-              <small className="error-comment visible mt-2 self-start text-soft-red dark:text-d-soft-red">
-                <i>{commentError}</i>
-              </small>
-              <div className="flex items-center justify-between pt-3">
-                {commentType == "send" && (
-                  <a
-                    href="#"
-                    className="cursor-pointer"
-                    aria-labelledby="send-userID"
-                  >
-                    <span className="sr-only" id="send-userID">
-                      Click this to view profile
-                    </span>
+    !isDeleting && (
+      <>
+        {
+          <div
+            key={id}
+            id={id}
+            className={`user-comment ${
+              commentError ? "animate-shake" : ""
+            }  space-y-4  rounded-lg bg-whitee  p-4 dark:bg-d-whitee`}
+          >
+            {commentType === "update" && (
+              <div className="flex items-center">
+                <a
+                  href="#"
+                  className="cursor-pointer rounded-full p-1 transition-all duration-500 hover:bg-light-gray dark:hover:bg-d-light-gray"
+                  aria-labelledby="update-userID"
+                >
+                  <span className="sr-only" id="update-userID">
+                    Click this to view profile
+                  </span>
+                  <figure className="flex items-center gap-4 pr-1  font-medium">
                     <img
                       className="w-8 rounded-full"
                       src={image}
-                      alt="user avatar"
+                      alt={`User Avatar ${username}`}
                     />
-                  </a>
-                )}
-                <div
-                  className={`cta-btns flex ${
-                    commentType === "update" ? "w-full" : ""
-                  } justify-between`}
-                >
-                  <button
-                    type="button"
-                    onClick={() => {
-                      setShowEmoji((prev) => !prev);
-                    }}
-                    className={`emojiBtn ${
-                      commentType === "send" && "hidden"
-                    } group btn btn-sm`}
-                    aria-labelledby="emojiBtnID"
-                  >
-                    <span className="sr-only" id="emojiBtnID">
-                      Click this button for emoji tray
-                    </span>
-                    <MdAddReaction className="fill-dark-blue text-xl transition  group-hover:fill-moderate-blue dark:fill-d-dark-blue dark:group-hover:fill-d-moderate-blue" />
-                  </button>
-
-                  <button
-                    type="submit"
-                    className={`${
-                      commentType === "send" && "h-fit px-7 py-3"
-                    } btn btn-sm w-fit bg-moderate-blue font-medium text-whitee transition-all hover:bg-moderate-blue hover:opacity-80 dark:bg-d-moderate-blue`}
-                    disabled={charCount === 0}
-                  >
-                    {commentType === "update" ? "Update" : "Send"}
-                  </button>
-                </div>
-              </div>
-            </form>
-          ) : (
-            <>
-              <p className="comment-actual whitespace-normal break-words text-grayish-blue dark:text-d-light-grayish-blue">
-                {
-                  // Handles censored text filtering if any
-                  commentType === "update" && filter.clean(comment)
-                }
-              </p>
-
-              <div className="btnss flex items-center justify-between">
-                {/* Handles voting functions*/}
-                <Vote score={score} isCurrentUser={isCurrentUser} />
-
-                {/* Handles icon buttons according to card type */}
-                {isCurrentUser ? (
-                  <div className="flex">
-                    <IconButton btnIndex="0" action={setisModified} />
-
-                    <IconButton btnIndex="1" action={setisModified} />
-                    <ConfirmationModal setisModified={setisModified} />
+                    <figcaption className="text-dark-blue dark:text-d-dark-blue">
+                      {username}
+                    </figcaption>
+                  </figure>
+                </a>
+                {isCurrentUser && (
+                  <div className="h-fit rounded-sm bg-moderate-blue px-1.5 text-sm text-whitee dark:bg-[#676ad1]">
+                    you
                   </div>
-                ) : (
-                  <IconButton btnIndex="2" action={setisModified} />
                 )}
+                <span className="pl-4 text-grayish-blue dark:text-white">
+                  {createdAt}
+                </span>
               </div>
-            </>
-          )}
-        </div>
-      )}
+            )}
 
-      {/* Handles adding reply card by current user in the reply box */}
-      {!isEmpty && (
-        <ReplyBox
-          isReplying={isReplying}
-          setisModified={setisModified}
-          replyCard={replyCards}
-        />
-      )}
-    </>
+            {/* Handles textarea editing */}
+            {isCurrentUser &&
+            (commentType === "update" ? isEditing : !isEditing) ? (
+              <form className="relative" onSubmit={updateComment}>
+                <textarea
+                  id="commentTextAreaID"
+                  name="comment"
+                  className={`comment-editing flex h-[10rem] w-full resize-none items-center justify-center overflow-auto rounded-md border dark:bg-[#383a40] ${
+                    commentError
+                      ? "border-red-500"
+                      : "focus:border-moderate-blue dark:focus:border-d-moderate-blue"
+                  }  p-2 text-grayish-blue caret-moderate-blue outline-none dark:text-white dark:caret-d-moderate-blue `}
+                  value={comment}
+                  onChange={editComment}
+                  onKeyDown={handleKeyDown}
+                  placeholder="Add a comment..."
+                  maxLength={MAX_CHARS_COMMENT}
+                  autoFocus
+                />
+
+                {/* Handles emoji picker */}
+                {showEmoji && (
+                  <div className="absolute right-2 top-full z-10 bg-very-light-gray shadow-xl  dark:bg-d-very-light-gray">
+                    <Picker
+                      data={emojidata}
+                      onEmojiSelect={addEmoji}
+                      emojiButtonSize={30}
+                      emojiSize={20}
+                      navPosition="bottom"
+                      previewPosition="none"
+                      skinTonePosition="none"
+                      emojiButtonColors={["skyblue", "lightgreen", "violet"]}
+                    />
+                  </div>
+                )}
+                <small className="absolute -top-4 right-3 text-slate-400 ">
+                  <i>{charCount}</i>
+                </small>
+                <small className="error-comment visible mt-2 self-start text-soft-red dark:text-d-soft-red">
+                  <i>{commentError}</i>
+                </small>
+                <div className="flex items-center justify-between pt-3">
+                  {commentType == "send" && (
+                    <a
+                      href="#"
+                      className="cursor-pointer"
+                      aria-labelledby="send-userID"
+                    >
+                      <span className="sr-only" id="send-userID">
+                        Click this to view profile
+                      </span>
+                      <img
+                        className="w-8 rounded-full"
+                        src={image}
+                        alt="user avatar"
+                      />
+                    </a>
+                  )}
+                  <div
+                    className={`cta-btns flex ${
+                      commentType === "update" ? "w-full" : ""
+                    } justify-between`}
+                  >
+                    <button
+                      type="button"
+                      onClick={() => {
+                        setShowEmoji((prev) => !prev);
+                      }}
+                      className={`emojiBtn ${
+                        commentType === "send" && "hidden"
+                      } group btn btn-sm`}
+                      aria-labelledby="emojiBtnID"
+                    >
+                      <span className="sr-only" id="emojiBtnID">
+                        Click this button for emoji tray
+                      </span>
+                      <MdAddReaction className="fill-dark-blue text-xl transition  group-hover:fill-moderate-blue dark:fill-d-dark-blue dark:group-hover:fill-d-moderate-blue" />
+                    </button>
+
+                    <button
+                      type="submit"
+                      className={`${
+                        commentType === "send" && "h-fit px-7 py-3"
+                      } btn btn-sm w-fit bg-moderate-blue font-medium text-whitee transition-all hover:bg-moderate-blue hover:opacity-80 dark:bg-d-moderate-blue`}
+                      disabled={charCount === 0}
+                    >
+                      {commentType === "update" ? "Update" : "Send"}
+                    </button>
+                  </div>
+                </div>
+              </form>
+            ) : (
+              <>
+                <p className="comment-actual whitespace-normal break-words text-grayish-blue dark:text-d-light-grayish-blue">
+                  {
+                    // Handles censored text filtering if any
+                    commentType === "update" && filter.clean(comment)
+                  }
+                </p>
+
+                <div className="btnss flex items-center justify-between">
+                  {/* Handles voting functions*/}
+                  <Vote
+                    commentType={type}
+                    score={score}
+                    isCurrentUser={isCurrentUser}
+                  />
+
+                  {/* Handles icon buttons according to card type */}
+                  {isCurrentUser ? (
+                    <div className="flex">
+                      <IconButton btnIndex="0" action={setisModified} />
+
+                      <IconButton btnIndex="1" action={setisModified} />
+                      <ConfirmationModal setisModified={setisModified} />
+                    </div>
+                  ) : (
+                    <IconButton btnIndex="2" action={setisModified} />
+                  )}
+                </div>
+              </>
+            )}
+          </div>
+        }
+
+        {/* Handles adding reply card by current user in the reply box */}
+        {!isEmpty && (
+          <ReplyBox
+            isReplying={isReplying}
+            setisModified={setisModified}
+            replyCard={replyCards}
+          />
+        )}
+      </>
+    )
   );
 }
