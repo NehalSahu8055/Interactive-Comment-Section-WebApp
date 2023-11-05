e179fb0e10b30bf279cc56014f4b6b9e5d1dea1f
Nehal_Sahu <ProfessionalNehal@outlook.com>
2023-11-05 17:29:59 +0530

    Fix heading issue

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -10,7 +10,7 @@ export default function Sidebar() {
       exit={{ opacity: 0 }}
       className="rounded-r-box fixed top-0 z-30 flex min-h-screen flex-col justify-between bg-whitee px-3 py-10  shadow-[2px_3px_7px_0px_#00000012] transition-all duration-300 ease-linear dark:bg-d-whitee dark:shadow "
     >
-      <h2 className="sr-only">Main Sidebar</h2>
+      <span className="sr-only">Main Sidebar</span>
 
       <UserSwitch />
       <ThemeToggle />
